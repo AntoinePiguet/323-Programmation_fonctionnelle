e76ccaa31284500815471c8dbfc2e6fcd733a2bb
pn01kbc <pn01kbc@eduvaud.ch>
2025-09-10 10:17:47 +0000

    Début de l'exo

--- PERSONNEL_APT/exos/mib-map/mib-lib/Program.cs
+++ PERSONNEL_APT/exos/mib-map/mib-lib/Program.cs
@@ -1,19 +1,3 @@
-var i18n = new Dictionary<string, string>()
-{
-    { "Pommes","Apples"},
-    { "Poires","Pears"},
-    { "Pastèques","Watermelons"},
-    { "Melons","Melons"},
-    { "Noix","Nuts"},
-    { "Raisin","Grapes"},
-    { "Pruneaux","Plums"},
-    { "Myrtilles","Blueberries"},
-    { "Groseilles","Berries"},
-    { "Tomates","Tomatoes"},
-    { "Courges","Pumpkins"},
-    { "Pêches","Peaches"},
-    { "Haricots","Beans"}
-};
 List<Product> products = new List<Product> {     
 new Product { Location = 1, Producer = "Bornand", ProductName = "Pommes", Quantity = 20,Unit = "kg", PricePerUnit = 5.50 },     
 new Product { Location = 1, Producer = "Bornand", ProductName = "Poires", Quantity = 16,Unit = "kg", PricePerUnit = 5.50 },     
@@ -23,10 +7,4 @@ new Product { Location = 2, Producer = "Dumont", ProductName = "Noix", Quantity 
 new Product { Location = 2, Producer = "Dumont", ProductName = "Raisin", Quantity = 6,Unit = "kg", PricePerUnit = 5.50 },     
 new Product { Location = 2, Producer = "Dumont", ProductName = "Pruneaux", Quantity = 13,Unit = "kg", PricePerUnit = 5.50 },     
 new Product { Location = 2, Producer = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", PricePerUnit = 5.50 },
-}
-
-string sellers = products.Where( p => p.Producer.substring(0,3) + "..." + p.Producer.Substring(p.Producer.Length - 1, 1));
-foreach(seller in sellers){
-	Console.WriteLine(seller)
 };
-Console.ReadLine();
